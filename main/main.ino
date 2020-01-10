ce1ab7cc5cf8badd12ac52588b4db36733bae583
kyte <kyte@bitsgoa.com>
2020-01-11 02:49:22 +0530

    Guide
    To functions to be included

--- main/main.ino
+++ main/main.ino
@@ -4,8 +4,38 @@
 
 void setup(){
 
+
+//WiFi initialisation + LED indication
+
+//nRF Initialisation + LED indication
+
+//WiFi connection check + LEDs
+
+//nRF plane parts testing and end the testing with button combination
+
+//Calibration of Sensors + leds
+
+//LED indicates battery level
 }
 
 void loop(){
+
+//Button press detection
+
+//MPU data collection and button feed from neighbour
+
+//Get the thing indicated in button combination done
+
+//MPU data collection from home
+
+//MPU data processing into pitch roll yaw
+
+//Get throttle data
+
+//Send all the data in different channels
+
+
+
+
 
 }
